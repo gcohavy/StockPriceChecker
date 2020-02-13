86cce9c316ae7e45b6d2afbeebf19acaefb234cf
Glitch (soft-bumper-7281x8i8zr) <none>
2020-02-13 18:06:05 +0000

    🍠🎒 Checkpoint ./routes/api.js:3976953/377

--- routes/api.js
+++ routes/api.js
@@ -13,12 +13,23 @@ var MongoClient = require('mongodb');
 
 const CONNECTION_STRING = process.env.DB; 
 
+var getJSON = function (url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+  xhr.onload = function () {
+    let status = xhr.status;
+    
+  }
+}
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
       var stock = req.query.stock;
       var like = req.query.like || false;
+
       var data = new XMLHttpRequest().open('GET', `https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`, false)
         .send(null);
       console.log(data);
